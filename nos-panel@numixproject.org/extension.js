2fd367267901fe4055febde11898bfe222b0ec78
Vladimir <vova.jameson2010@yandex.ru>
2014-03-30 13:38:35 +0400

    Fixed bug with start and stop extension

--- nos-panel@numixproject.org/extension.js
+++ nos-panel@numixproject.org/extension.js
@@ -1,20 +1,29 @@
-/*This panel was developed by Vladimir Khrustaliov for the Numix Project,but is not ended.
-Extension's version: 0.1;
+/*
+									Release Notes: 
+Description of Revision: This panel was developed by Vladimir Khrustaliov for the Numix Project,but is not ended.
+Extension's version: 0.2;
+Recent changes in the new version: 
+	1.Code has been simplified. 
+	2.Fixed bug with on / off extensions in gnome-tweak-tool. 
+	3.It is evident that css-file is not loaded in the code. However, GNOME will connect automatically. Almost all design changes occur in the 		file stylesheet.css. In any case does not load css-files via js - script. Otherwise, the extension will not turn off after gnome-tweak-tool. 		4.Images folder has been deleted. 
+	5.Css-file has been simplified compared to the previous. 
+	5.Brackets in the code will not touch. Since I find it easier "to compile it in the head", to find and remove bugs. 
+
+Waiting for feedback.
+
 Connect with me on:
 VK - http://vk.com/zzz_jameson_zzz
 Facebook - https://www.facebook.com/profile.php?id=100006124748177
 Google + - https://plus.google.com/u/0/105238933702957776242/posts
-E-mail: vova.jameson2010@yandex.ru*/
+E-mail: vova.jameson2010@yandex.ru
+*/
 
 const St = imports.gi.St;
-const Shell = imports.gi.Shell;
 const Main = imports.ui.main;
-const Lang = imports.lang;
 
 function init(extensionMeta)
 {
 	let defaultStylesheet = Main.getThemeStylesheet();
-	let patchStylesheet = extensionMeta.path + '/stylesheet.css';//Connect the css-file, which contains the design panel
   
 	Main.overview.connect('showing', opacityOn);//When we open the overview, function is performed "opacityOn ()"
 	Main.overview.connect('hiding', opacityOff);//When we close the overview, function is performed "opacityOff ()"
@@ -27,46 +36,25 @@ function opacityOff()
 {
 	imports.ui.main.panel.actor.set_style('background-color: #2d2d2d');//Remove the transparent background
 }
-
-function enable() 
+function std()
 {
-//Do not touch anything here. This code works on magic!
-
-	//imports.ui.main.panel.statusArea.appMenu.actor.hide();
-	//imports.ui.main.panel.statusArea.activities.actor.hide();
-	//imports.ui.main.panel.statusArea.a11y.actor.hide();
-	//imports.ui.main.panel.statusArea.userMenu.actor.hide();
-	//imports.ui.main.panel.statusArea.dateMenu.actor.hide();
-	let themeContext = St.ThemeContext.get_for_stage(global.stage);
-	let theme = new St.Theme 
-	({ 
-		application_stylesheet: patchStylesheet
-		/*theme_stylesheet: "/usr/share/gnome-shell/theme/gnome-shell.css"*/ });
-		try 
-		{ 
-			themeContext.set_theme(theme); 
-		} 
-		catch (e) 
-		{ 
-			global.logError('Stylesheet parse error: ' + e); 
-		}
+	imports.ui.main.panel.actor.set_style('background-color: black');//Establish a black background panel while executing
 }
 
-function disable() 
+function enable() //What happens if the extension starts.
 {
-//Do not touch anything here. This code works on magic!
-	let themeContext = St.ThemeContext.get_for_stage(global.stage);
-	let theme = new St.Theme 
-	({ 
-		theme_stylesheet: defaultStylesheet 
-	});
-	try 
-	{ 
-		themeContext.set_theme(theme); 
-	} 
-	catch (e) 
-	{ 
-		global.logError('Stylesheet parse error: ' + e); 
-	}
+
+	opacityOff();//Specify the desired color panel.
+	Main.overview.connect('showing', opacityOn);//When we open the overview, function is performed "opacityOn ()"
+	Main.overview.connect('hiding', opacityOff);//When we close the overview, function is performed "opacityOff ()"
+
+}
+
+function disable()//What happens if the extension is turned off.
+{
+	std();//Return standard color panel.
+	Main.overview.connect('showing', std);//When we open the overview, function is performed "std ()"
+	Main.overview.connect('hiding', std);//When we close the overview, function is performed "std ()"
+
 }
 

de5ff52ae17798806d681a549b36e902535895d2
Vladimir <vova.jameson2010@yandex.ru>
2014-04-20 10:20:31 +0400

    Lagacy indicators
    No comments yet.

--- extension.js
+++ extension.js
@@ -1,63 +1,169 @@
-/* Numix/Ozon Project 2014
- * 
- * Extension's version: 0.3
- * 
- * 0.2 Changes:
- * 	1.Code has been simplified.
- * 	2.Fixed bug with on / off extensions in gnome-tweak-tool.
- * 	3.It is evident that css-file is not loaded in the code.
- * 		However, GNOME will connect automatically.
- * 		Almost all design changes occur in the file stylesheet.css. 
- * 		In any case DO NOT load css-files via js - script. 
- * 		Otherwise, the extension will not turn off after gnome-tweak-tool.
- * 	4.Images folder has been deleted.
- * 	5.Css-file has been simplified compared to the previous.
- * 
- * 0.3 Changes: 
- * 	1. Embrace CamelCase for clear function naming and readability
- * 	2. Added Connect Handler ID's we can grab to disconnect in a clean way
- * 	3. Separate intial recoloring of panel from transparency functions
- * 	4. Added constant Panel Object for the sake of simplicity
- * 	5. Cleaned init cause it was all duplicated code from enable (and its supposed to be empty anyways)
- * 	6. Cleaned up credits
- * 
- * Authors: 
- * 	Vladimir Khrustaliov (vova.jameson2010@yandex.ru|https://plus.google.com/u/0/105238933702957776242/)
- * 	Joern Konopka (cldx3000@gmail.com)
- *  Please add yourseld to the list, we should keep an Authors file for this maybe
- */
-
+const Clutter = imports.gi.Clutter;
+const Shell = imports.gi.Shell;
 const Main = imports.ui.main;
-const Panel = imports.ui.main.panel;
+const Lang = imports.lang;
+const PanelMain = imports.ui.main.panel;
+const Panel = imports.ui.panel;
+const PanelMenu = imports.ui.panelMenu;
+const Meta = imports.gi.Meta;
+const Mainloop = imports.mainloop;
+const NotificationDaemon = imports.ui.notificationDaemon;
+const St = imports.gi.St;
 
-function init(){
-	/*  do nothing  */
+let trayAddedId = 0;
+let trayRemovedId = 0;
+let getSource = null;
+let icons = [];
+let  button, legacyIcon, setIcon, buttonEvenet;
+var me = 'shouldBeHide';
+
+function init() {
+    getSource = Lang.bind(Main.notificationDaemon, NotificationDaemon.NotificationDaemon.prototype._getSource);
+    
+    button = new St.Bin({ style_class: 'panel-button',
+			      reactive: true,
+		              can_focus: true,
+		              x_fill: true,
+		              y_fill: false,
+		              track_hover: true });
+	legacyIcon = new St.Icon({ icon_name: 'pane-hide-symbolic',
+		             style_class: 'system-status-icon' });
+	button.set_child(legacyIcon);
 }
 
-function setPanelTransparent(){
-	Panel.actor.set_style('background-color: rgba(0,0,0,0)');
+function enable() {
+    moveToTop();
+    
+    
 }
 
-function unsetPanelTransparent(){
-	Panel.actor.set_style('background-color: #2d2d2d');
+function createSource (title, pid, ndata, sender, trayIcon) { 
+  if (trayIcon) {
+    onTrayIconAdded(this, trayIcon, title);
+    return null;
+  }
+
+  return getSource(title, pid, ndata, sender, trayIcon);
+};
+
+function onTrayIconAdded(o, icon, role) {
+    let wmClass = icon.wm_class ;
+    if (NotificationDaemon.STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass] !== undefined)
+        return;
+
+    let buttonBox = new PanelMenu.ButtonBox();
+    let box = buttonBox.actor;
+    let parent = box.get_parent();
+ 
+    icon.set_size(Panel.PANEL_ICON_SIZE, Panel.PANEL_ICON_SIZE);
+    box.add_actor(icon);
+
+    icon.reactive = true;
+    icon._clicked = icon.connect('button-release-event', function(actor, event) {
+        icon.click(event);
+    });
+
+    if (parent)
+        parent.remove_actor(box);
+
+    icons.push(icon);
+    Main.panel._rightBox.insert_child_at_index(button, 0);
+    Main.panel._rightBox.insert_child_at_index(box, 0);
+    
+    /* Fixme: HACK */
+    
+    buttonEvent = button.connect('button-press-event', function(){
+    
+    if(me == 'shouldBeHide'){
+		
+		box.hide();        
+		
+		setIcon = new St.Icon({ icon_name: 'pane-show-symbolic',
+		             	     	style_class: 'system-status-icon' });
+		button.remove_child(legacyIcon);
+		button.set_child(setIcon);
+		me = 'shouldBeShow';	
+
+	}
+	else if(me == 'shouldBeShow'){
+       
+    box.show();
+	        
+	        
+		setIcon = new St.Icon({ icon_name: 'pane-hide-symbolic',
+		             	     	style_class: 'system-status-icon' });
+		button.remove_child(legacyIcon);
+		button.set_child(setIcon);
+		me = 'shouldBeHide';
+		
+	}});
+	
+   
+    
 }
 
-function setPanelSolidColor(){
-	Panel.actor.set_style('background-color: #2d2d2d');
+function onTrayIconRemoved(o, icon) {
+    let parent = icon.get_parent();
+    parent.destroy();
+    icon.destroy();
+    icons.splice(icons.indexOf(icon), 1);
 }
 
-function setPanelDefaultColor(){
-	Panel.actor.set_style('background-color: black');
+function moveToTop() {
+    Main.notificationDaemon._trayManager.disconnect(Main.notificationDaemon._trayIconAddedId);
+    Main.notificationDaemon._trayManager.disconnect(Main.notificationDaemon._trayIconRemovedId);
+    trayAddedId = Main.notificationDaemon._trayManager.connect('tray-icon-added', onTrayIconAdded);
+    trayRemovedId = Main.notificationDaemon._trayManager.connect('tray-icon-removed', onTrayIconRemoved);
+    
+    Main.notificationDaemon._getSource = createSource;
+
+    let toDestroy = [];
+    for (let i = 0; i < Main.notificationDaemon._sources.length; i++) {
+        let source = Main.notificationDaemon._sources[i];
+        if (!source.trayIcon)
+            continue;
+        let parent = source.trayIcon.get_parent();
+        parent.remove_actor(source.trayIcon);
+        onTrayIconAdded(this, source.trayIcon, source.initialTitle);
+        toDestroy.push(source);
+    }
+
+     for (let i = 0; i < toDestroy.length; i++) {
+        toDestroy[i].destroy();
+     }
 }
 
-function enable(){ 
-	setPanelSolidColor();
-	this.showingHandler = Main.overview.connect('showing', setPanelTransparent);
-	this.hidingHandler = Main.overview.connect('hiding', unsetPanelTransparent);
+function moveToTray() {
+    if (trayAddedId != 0) {
+        Main.notificationDaemon._trayManager.disconnect(trayAddedId);
+        trayAddedId = 0;
+    }
+
+    if (trayRemovedId != 0) {
+        Main.notificationDaemon._trayManager.disconnect(trayRemovedId);
+        trayRemovedId = 0;
+    }
+    
+    Main.notificationDaemon._trayIconAddedId = Main.notificationDaemon._trayManager.connect('tray-icon-added',
+                                                Lang.bind(Main.notificationDaemon, Main.notificationDaemon._onTrayIconAdded));
+    Main.notificationDaemon._trayIconRemovedId = Main.notificationDaemon._trayManager.connect('tray-icon-removed', 
+                                                Lang.bind(Main.notificationDaemon, Main.notificationDaemon._onTrayIconRemoved));
+
+    Main.notificationDaemon._getSource = getSource;
+
+    for (let i = 0; i < icons.length; i++) {
+        let icon = icons[i];
+        let parent = icon.get_parent();
+        icon.disconnect(icon._clicked);
+        icon._clicked = undefined;
+        parent.remove_actor(icon);
+        parent.destroy();
+        Main.notificationDaemon._onTrayIconAdded(Main.notificationDaemon, icon);
+    }
+    
+    icons = [];
 }
 
-function disable(){
-	setPanelDefaultColor();
-	Main.overview.disconnect(this.showingHandler);
-	Main.overview.disconnect(this.hidingHandler);
+function disable() {
+    
 }
